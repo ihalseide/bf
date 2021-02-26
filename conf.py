785724b9e4b101ccda1d273b98e3d867f8cfd055
ihalseide <imitatio-ignotum@protonmail.com>
2021-02-26 15:51:29 -0700

    Add config file for future import use

---
+++ conf.py
@@ -0,0 +1,11 @@
+# Customizable settings 
+
+# How many cells to print out in debug
+num_debug_cells = 20     
+
+# Number of memory cells
+num_cells = 65536 
+
+# Input End of File settings
+EOF_is_overwrite = False 
+EOF_value = 0            
