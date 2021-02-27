ff81c3f538e5e4d4064a8cff71293a17af788c7f
ihalseide <imitatio-ignotum@protonmail.com>
2021-02-26 18:36:41 -0700

    Add command line arguments to the interpreter

--- bf.py
+++ bf.py
@@ -5,89 +5,102 @@
 import sys, argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('file')
 parser.add_argument('-num_cells', default=65535, type=int, help='number of memory cells')
+parser.add_argument('-file', help='the bf program input file, default is to read stdin')
+parser.add_argument('-debug', action='store_true')
+parser.add_argument('--EOF_value', type=int, default=None, help='End Of File value, default is not to write any value upon EOF')
 args = parser.parse_args()
 
-num_cells = args.num_cells
-with open(args.file, 'r') as f:
-    program = f.read() 
+if args.EOF_value is not None and args.EOF_value not in range(255):
+    raise ValueError('EOF_value must be in range(0, 255)')
 
-memory = bytearray([0 for x in range(num_cells)])
-cp = 0              # cell/memory pointer
-ip = 0              # intruction pointer
-cycles = 0          # keep track of absolute number of instructions processed
+num_cells = args.num_cells
+if args.file is None:
+    # Read program from stdin until a '!'
+    program = ''
+    while (c := sys.stdin.read(1)) not in ('', '!'):
+        program += c
+else:
+    # Read program from file
+    with open(args.file, 'r') as f:
+        program = f.read() 
+
+def error (*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
+    sys.exit(-1)
 
 # Preprocess the brackets matches to increase efficiency a little
 matches_start = {}  # maps '[' to ']'
 matches_end = {}    # maps ']' to '['
-for index, char in enumerate(program):
+for i, char in enumerate(program):
     if '[' == char:
-        depth = 1
+        # Search for the matching ']'
+        depth = 0
         match = None
-        for i in range(index + 1, len(program)):
-            char2 = program[i]
+        for i2, char2 in enumerate(program[i:]):
             if '[' == char2:
                 depth += 1
             elif ']' == char2:
                 depth -= 1
                 if depth == 0:
-                    match = i
+                    match = i + i2
                     break
-        if match is not None:
-            matches_start[index] = match
-            matches_end[match] = index
-        else:
-            raise SyntaxError('unmatched "[" at index %d' % index)
+        if match is None:
+            error('syntax error:', 'unmatched "[" at index %d' % i)
+        matches_start[i] = match
+        matches_end[match] = i 
     elif ']' == char:
-        if matches_end.get(index) is None:
-            raise SyntaxError('unmatched "]" at index %d' % index)
+        # Make sure there was a matching '['
+        if matches_end.get(i) is None:
+            error('syntax error:', 'unmatched "]" at index %d' % i)
 
-# Start running the program
-while ip < len(program): 
-    char = program[ip] 
-    if char == '>':
-        # Move the pointer to the right
-        cp += 1
-    elif char == '<':
-        # Move the pointer to the left
-        cp -= 1
-        if cp < 0:
-            raise Exception('pointer moved to negative location, at program index %d' % ip)
-    elif char == '+':
-        # Increment the cell at the pointer
-        n = memory[cp]
-        n = (n + 1) % 256
-        memory[cp] = n
-    elif char == '-':
-        # Decrement the cell at the pointer
-        n = memory[cp]
-        n = (n - 1) % 256
-        memory[cp] = n
-    elif char == '.':
-        # Output the character at the cell pointer
-        c = chr(memory[cp])
-        sys.stdout.write(c)
-    elif char == ',':
-        # Input a character and store it in the cell at the pointer
-        c = sys.stdin.read(1)
-        if c:
-            memory[cp] = c
-        elif EOF_is_overwrite: # `c` will be '' when EOF in Python
-            memory[cp] = EOF_value
-    elif char == '[':
-        # Jump past the matching ] if the cell at the pointer is 0
-        if memory[cp] == 0: 
-            ip = matches_start[ip]
-    elif char == ']':
-        # Jump back to the matching [ if the cell at the pointer is not 0
-        if memory[cp] != 0:
-            ip = matches_end[ip]
-    elif '#' == char:
-        # Debug: print first few cells 
-        print('\n[%s...]' % ','.join('%x' % x for x in memory[:num_debug_cells]))
-    elif '!' == char:
-        # Extension: separate code from input
-        # Since this is an interpreter, just take it to mean "halt"
-        break 
-    ip += 1
+memory = bytearray([0 for x in range(num_cells)])
+cp = 0              # cell/memory pointer
+ip = 0              # intruction pointer
+
+try:
+    # Start running the program
+    while ip < len(program): 
+        char = program[ip] 
+        if char == '>':
+            # Move the pointer to the right
+            cp += 1
+        elif char == '<':
+            # Move the pointer to the left
+            cp -= 1
+        elif char == '+':
+            # Increment the cell at the pointer
+            n = memory[cp]
+            n = (n + 1) % 256
+            memory[cp] = n
+        elif char == '-':
+            # Decrement the cell at the pointer
+            n = memory[cp]
+            n = (n - 1) % 256
+            memory[cp] = n
+        elif char == '.':
+            # Output the character at the cell pointer
+            c = chr(memory[cp])
+            sys.stdout.write(c)
+        elif char == ',':
+            # Input a character and store it in the cell at the pointer
+            c = sys.stdin.read(1) # Note: will be str('') upon EOF in Python
+            if c:
+                memory[cp] = ord(c)
+            elif args.EOF_value is not None: 
+                memory[cp] = args.EOF_value
+        elif char == '[':
+            # Jump past the matching ] if the cell at the pointer is 0
+            if memory[cp] == 0: 
+                ip = matches_start[ip]
+        elif char == ']':
+            # Jump back to the matching [ if the cell at the pointer is not 0
+            if memory[cp] != 0:
+                ip = matches_end[ip]
+        elif '#' == char:
+            # Debug: print first few cells 
+            print('\n[%s...]' % ','.join('%x' % x for x in memory[:num_debug_cells]))
+        ip += 1
+except Exception as e:
+    print('bf: at index %d with pointer at cell %d' % (ip, cp))
+    raise e
