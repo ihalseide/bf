e31861e2e16e29e284dd170d960009dc3a8ff54f
ihalseide <imitatio-ignotum@protonmail.com>
2021-02-26 17:16:19 -0700

    Fix imports and refactor in bfc.py

--- bfc.py
+++ bfc.py
@@ -2,24 +2,27 @@
 
 # bf compiler
 
-import argparse
-from conf import *
+import sys, argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file')
-parser.add_argument('-comment', action='store_true', help='whether to generate comments in the C output')
+parser.add_argument('-debug', action='store_true', help='whether to generate comments in the C output')
 parser.add_argument('-num_cells', default=65535, type=int, help='number of memory cells')
 args = parser.parse_args()
 
-comment_p = args.comment
-num_cells = args.num_cells
 with open(args.file, 'r') as f:
     program = f.read() 
+
+def print_line (*args, **kwargs):
+    emit('    ' * indent, *args, sep='', end='\n', *kwargs)
 
 # Emit the C Preamble
 print(
 '''\
-/* brainfuck code converted to C */
+/* brainfuck code
+ * from the file "%s"
+ * compiled with "%s"
+ */
 
 #include <stdio.h>
 
@@ -28,10 +31,8 @@ char * p;
 
 int main (int argc, int ** argv) {
     p = &mem;\
-''' % num_cells)
+''' % (args.file, parser.prog, args.num_cells))
 
-indent = 1 
-emit = lambda string: print('    ' * indent, string, sep='') 
 
 # Keep track of when brackets start and end
 last_lbracket = 0
@@ -41,8 +42,8 @@ try:
     prog = enumerate(iter(program))
     index, char = next(prog)
     while True:
-        if comment_p:
-            emit('// %s (index %d)' % (char, index))
+        if args.debug:
+            emit_line('// %s (index %d)' % (char, index))
         if char in '<>':
             # Move the pointer to the right or left
             num = 0
@@ -54,7 +55,7 @@ try:
                     num += 1
                 index, char = next(prog)
             if num != 0:
-                emit('p += %d;' % num)
+                emit_line('p += %d;' % num)
             del num
             continue
         elif char in '+-':
@@ -68,40 +69,43 @@ try:
                     num += 1
                 index, char = next(prog)
             if num != 0:
-                emit('*p += %d;' % num)
+                emit_line('*p += %d;' % num)
             del num
             continue
         elif '.' == char:
             # Output the character at the cell pointer
-            emit('putchar(*p);')
+            emit_line('putchar(*p);')
         elif ',' == char:
             # Input a character and store it in the cell at the pointer
             if EOF_is_overwrite:
-                emit('*p = getchar();')
+                emit_line('*p = getchar();')
             else:
                 raise NotImplemented() 
         elif '[' == char:
             # Jump past the matching ] if the cell at the pointer is 0
             last_lbracket = index
-            emit('while (*p) {')
+            emit_line('while (*p) {')
             indent += 1
         elif ']' == char :
             # Jump back to the matching [ if the cell at the pointer is not 0
             last_rbracket = index
             indent -= 1
-            emit('}')
+            emit_line('}')
         elif '#' == char:
             # Debug breakpoint
             pass
         elif '!' == char:
             # Stop compiling
             break 
+        else:
+            # Unknown char, so skip
+            pass
         index, char = next(prog)
 except StopIteration:
     pass
 
 indent -= 1
-emit('}')
+emit_line('}')
 
 if indent > 0:
     raise SyntaxError('unmatched "[" at index %d' % last_rbracket)
